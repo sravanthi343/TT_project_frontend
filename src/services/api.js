132fd8262924de27f397696c2c839a05415c8587
sravanthi343 <sandelasravanthi343@gmail.com>
2026-03-14 22:24:40 +0530

    update backend for deployment

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
+const BASE_URL = import.meta.env.VITE_API_URL ||"https://tt-project-backend.onrender.com/api/users";
 //https://problem-reporting-tracking.onrender.com
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080/api';
+//const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080/api';
 
 const api = axios.create({
   baseURL: BASE_URL,
